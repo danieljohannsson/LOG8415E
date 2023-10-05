3524b6cf64573f8271da819b0b44131cafad45f1
Vanessa Nguyen <va4274ph-s@student.lu.se>
2023-10-05 12:57:05 -0400

    create instances + main

--- lab1/Main.py
+++ lab1/Main.py
@@ -2,22 +2,37 @@ import boto3
 import util
 import load_balancer
 import ec2_instances
+import web_requests
+import time
 
 def main():
     ec2_client = boto3.client('ec2')
     elb_client = boto3.client('elbv2')
     avZones = ['us-east-1a', 'us-east-1b', 'us-east-1c', 'us-east-1d', 'us-east-1e']
-    print("client criado")
+    print("Clients created!")
     subnets = util.fetch_subnet(ec2_client, avZones)
-    lbArn, tg1Arn, tg2Arn, sgId = load_balancer.create_lb_listener(subnets, ec2_client, elb_client)
-    print('lb criado')
-    C1_ids, C2_ids = ec2_instances.EC2_instances(tg1Arn, tg2Arn, subnets, ec2_client, elb_client)
-    print('instancias criadas')
-    util.shut_down_instances(ec2_client, C1_ids)
-    util.shut_down_instances(ec2_client, C2_ids)
+
+    Instances_ids = ec2_instances.EC2_instances( subnets, ec2_client)
+    print('Instances created!')
+
+    lbArn, tg1Arn, tg2Arn, security_groups = load_balancer.create_lb_listener(subnets, ec2_client, elb_client)
+    print('LB created!')
+    time.sleep(120)
+
+    ec2_instances.attach_instances(tg1Arn, tg2Arn, elb_client, 1, Instances_ids)
+    ec2_instances.attach_instances(tg1Arn, tg2Arn, elb_client, 2, Instances_ids)
+    print('Attached instances!')
+    
+    time.sleep(120)
+    web_requests.requests_main()
+
+
+    util.shut_down_instances(ec2_client, Instances_ids)
     util.shut_down_load_balancer(elb_client, lbArn, tg1Arn, tg2Arn)
-    # util.shut_down_security_group(sgId)
-    print('tudo deletado')
+    time.sleep(120)
+    #DELETE SECURITY GROUP
+    #DELETE KEYPAIR
+    print('All terminated')
 
     return
 
--- lab1/ec2_instances.py
+++ lab1/ec2_instances.py
@@ -1,10 +1,9 @@
 import boto3
 
-def EC2_instances(tg1, tg2, subnets, ec2_client, elb_client):
+def EC2_instances(subnets, ec2_client):
     ec2_client = boto3.client('ec2')
-    elb_client = boto3.client('elbv2')
-    amiId = 'ami-03a6eaae9938c858c'
-
+    amiId = 'ami-053b0d53c279acc90'
+    instanceID = []
     
     def create_key_pair(KPName):
         ec2_client.create_key_pair(KeyName=KPName)
@@ -12,67 +11,47 @@ def EC2_instances(tg1, tg2, subnets, ec2_client, elb_client):
     
     # open instance script
     def launch_script():
-        with open("instanceScript.sh", "r") as file:
+        with open("./lab1/flask/instanceScript.sh") as file:
             script = file.read()
         return script
     
     # ec2 instance launcher
-    def launch_instance(KPName, Itype, subnet):
+    def launch_instance(Itype, subnet, KeyName):
         response = ec2_client.run_instances(
                     ImageId=amiId,
                     MinCount=1,
                     MaxCount=1,
                     InstanceType=Itype,
-                    KeyName=create_key_pair(KPName),
+                    KeyName=KeyName,
                     SubnetId=subnet,  
                     UserData=launch_script()
         )
-        return response['Instances'][0]['InstanceId']
+        return response['Instances'][0]['InstanceId'] 
     
-    def attach_instances(type, cluster, port=80):
-        ec2_resource = boto3.resource('ec2')
-        instancesIds = []
-        flag = 1
-        if cluster == 1:
-            for i in range(0, len(subnets)):
-                instancesIds.append(launch_instance(f'kp_Cluster-{cluster}_Instance-{i+1}', type, subnets[i]))
-            
-            while(flag):
-                for i in instancesIds:
-                    if ec2_resource.Instance(i).state['Name'] != 'running':
-                        flag = 1
-                        break
-                    else:
-                        flag = 0
-            
-            elb_client.register_targets(
-                TargetGroupArn = tg1,
-                Targets=[{'Id': instancesIds[0], 'Port':port}, {'Id': instancesIds[1], 'Port':port},
-                         {'Id': instancesIds[2], 'Port':port}, {'Id': instancesIds[3], 'Port':port},
-                         {'Id': instancesIds[4], 'Port':port}
-                        ]
-            )
-        else:
-            for i in range(0, len(subnets)-1):
-                instancesIds.append(launch_instance(f'kp_Cluster-{cluster}_Instance-{i+1}', type, subnets[i]))
 
-            while(flag):
-                for i in instancesIds:
-                    if ec2_resource.Instance(i).state['Name'] != 'running':
-                        flag = 1
-                        break
-                    else:
-                        flag = 0
+    def launch_instances(type, number, KPName):
+        for i in range(number):
+            instanceID.append(launch_instance(type,subnets[i], KPName))
+    
+    KPName=create_key_pair("MyKey2")
+    launch_instances("t2.large", 4, KPName)
+    launch_instances("m4.large", 5, KPName)
+    return instanceID
 
-            elb_client.register_targets(
-                TargetGroupArn = tg2,
-                Targets=[{'Id': instancesIds[0], 'Port':port}, {'Id': instancesIds[1], 'Port':port},
-                         {'Id': instancesIds[2], 'Port':port}, {'Id': instancesIds[3], 'Port':port},
-                        ]
-            )
-        
-        return instancesIds
-
-    C1_ids = attach_instances('t2.large', 1)
-    C2_ids = attach_instances('m4.large', 2)
-    return  C1_ids, C2_ids
+def attach_instances(tg1, tg2, elb_client, cluster, instancesIds,port=80):
+    elb_client = boto3.client('elbv2')
+   
+    if cluster == 1:
+        elb_client.register_targets(
+            TargetGroupArn = tg1,
+            Targets=[{'Id': instancesIds[0], 'Port':port}, {'Id': instancesIds[1], 'Port':port},
+                        {'Id': instancesIds[2], 'Port':port}, {'Id': instancesIds[3], 'Port':port}
+                    ]
+        )
+    else:
+        elb_client.register_targets(
+            TargetGroupArn = tg2,
+            Targets=[{'Id': instancesIds[4], 'Port':port}, {'Id': instancesIds[5], 'Port':port},
+                        {'Id': instancesIds[6], 'Port':port}, {'Id': instancesIds[7], 'Port':port}, {'Id': instancesIds[8], 'Port':port}
+                    ]
+        )
