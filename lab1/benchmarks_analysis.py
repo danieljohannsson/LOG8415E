4fda9c8ad4b71f1ebd7c3ded7d6a6ac5e33a95c3
Sofia Ingigerth Canas Urbina <84815840+SICU2001@users.noreply.github.com>
2023-10-07 19:02:45 -0400

    Update benchmarks_analysis.py

--- lab1/benchmarks_analysis.py
+++ lab1/benchmarks_analysis.py
@@ -4,49 +4,79 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-    cloudwatch = boto3.client('cloudwatch', region_name = "us-east-1")
-
-
+    region_name="us-east-1"
+    client = boto3.client('cloudwatch', region_name = region_name)
+    namespace = 'AWS/ApplicationELB'
     # Selected metrics and stat using the following format: 'MetricName': 'Stat'
-    metrics_stat = {
-        'HTTPCode_ELB_4XX_Count': 'Sum', 
-        'HTTPCode_ELB_5XX_Count': 'Sum', 
-        'RequestCountPerTarget': 'Sum', 
+    metric_names_stats = {
+        'HTTPCode_ELB_4XX_Count': 'Sum',
+        'HTTPCode_ELB_5XX_Count': 'Sum',
+        'RequestCountPerTarget': 'Sum',
         'TargetResponseTime': 'Average',
-        'HTTPCode_Target_2XX_Count': 'Sum', 
-        'HTTPCode_Target_4XX_Count': 'Sum',
+        'HTTPCode_Target_2XX_Count': 'Sum',
+        'HTTPCode_Target_4XX_Count': 'Sum'
     }
+    period = 5  # in seconds
+    time_offset = 30 * 60  # last 30 minutes in seconds
+
+    # Determine the unit based on the metric name
+    def get_unit(metric_name):
+        if metric_name in ['HTTPCode_ELB_4XX_Count','HTTPCode_ELB_5XX_Count','HTTPCode_Target_2XX_Count','HTTPCode_Target_4XX_Count']:
+            return 'Count'
+        elif metric_name == 'TargetResponseTime':
+            return 'Seconds'  # or whatever unit is appropriate for response time
+        else:
+            return 'None'  # No unit
+
+    session = boto3.Session(region_name=region_name)
+    elbv2_client = session.client('elbv2')
+
+    resp = elbv2_client.describe_target_groups()
+
+    load_balancer = resp['TargetGroups'][0]['LoadBalancerArns'][0]
+    target_group1 = resp['TargetGroups'][0]['TargetGroupArn']
+    target_group2 = resp['TargetGroups'][1]['TargetGroupArn']
+
+    tgarray1 = target_group1.split(':')
+    tgstring1 = tgarray1[-1]
+
+    tgarray2 = target_group2.split(':')
+    tgstring2 = tgarray2[-1]
+
+    lbarray = load_balancer.split(':')
+    lbstring = lbarray[-1]
+    lbarray2 = lbstring.split('/')
+    lbstring2 = lbarray2[1] + '/' + lbarray2[2] + '/' + lbarray2[3]
 
     # creating metric queries
-    def create_queries():
-        metric_queries = []
-        for id, metric in enumerate(metrics_stat.keys()):
-            namespace_raw = cloudwatch.list_metrics(MetricName=metric)
-            namespace = namespace_raw['Metrics'][0]['Namespace']
+    metric_queries = []
+    for metric_idx, (metric_name, stat) in enumerate(metric_names_stats.items()):
+        for tg_idx, tg in enumerate([tgstring1, tgstring2]):
+            # Using indices for generating Id
+            id_string = f"m{metric_idx}_tg{tg_idx}"
             metric_queries.append({
-                    'Id': f'metric_{id}',
-                    'MetricStat': {
-                        'Metric': {'MetricName':metric, 'Namespace':namespace},
-                        'Period': 30,
-                        'Stat': metrics_stat[metric]
-                    }
-                })
+                'Id': id_string,
+                'MetricStat': {
+                    'Metric': {
+                        'Namespace': namespace,
+                        'MetricName': metric_name,
+                        'Dimensions': [
+                            {"Name": "TargetGroup", "Value": tg},
+                            {"Name": "LoadBalancer", "Value": lbstring2}
+                        ]
+                    },
+                    'Period': period,
+                    'Stat': stat,
+                    'Unit': get_unit(metric_name)
+                }
+            })
 
-        # Launching the queries
-        response = cloudwatch.get_metric_data(
-            MetricDataQueries=metric_queries,
-            StartTime=datetime.utcnow() - timedelta(minutes=30),
-            EndTime=datetime.utcnow()
-        )
+    # Launching the queries
+    response = client.get_metric_data(
+        MetricDataQueries=metric_queries,
+        StartTime=datetime.utcnow() - timedelta(seconds=time_offset),
+        EndTime=datetime.utcnow()
+    )
 
-        data_cluster = response["MetricDataResults"]
-
-        # visualizing the query results
-        print(data_cluster)
-
-        return 
-
-
-
-    create_queries()
-    return
+    # returning for visualizing the query results
+    return response["MetricDataResults"]
